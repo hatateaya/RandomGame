b9fdc46bb8ed1a0a2cfb5b8a716053338c77f6f0
Hatate Aya <99482521+hatateaya@users.noreply.github.com>
2024-02-22 11:18:50 +0800

    Add project files.

---
+++ RandomGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
